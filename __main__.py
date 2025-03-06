6617c822e57c7a25527c956788370b2c02ed75fa
Santiago Echeverria <fabio.echeverria.perpetua@mi.unc.edu.ar>
2025-03-06 17:32:38 -0300

    TODO: Turno variante y variaciones ff, tt, ft, tf

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 import random
-import examples.example_race as example
+import examples.example_coin_flipper as example
 from parser import Parser
 from qlearning.main import Q_table
 from smartsampling.main import Transformer as SmartSampling
+from training_output import TrainingOutput
+
 
 SMART_SAMPLING_N = 2**10
 Q_TABLE_ITERATIONS = 10000
@@ -19,8 +21,8 @@ END_COMPARATION = 1000
 Q_LEARNING_RESET = False
 SMART_SAMPLING_CONSTANT = True
 
-OUTPUT_FILE = 'StochGameTesis/output/race_ft.txt'
-
+OUTPUT_FILE = 'coin_ft'
+OUTPUTTER = TrainingOutput(OUTPUT_FILE)
 
 # Version Q-learning resetea con cada loop de Smart Sampling
 # Version Smart Sampling usa un z aleatorio en cada movimiento de Q-learning
@@ -28,10 +30,6 @@ Q_TABLE.init_q_table(parser.get_all_posibilities())
 
 iterarion = 1
 while SMART_SAMPLING_N > 1:
-    result_qlearning = 0
-    result_smartsampling = 0
-    result_draw = 0
-
     # Correr Q-learning donde Smart Sampling elige con probabilidad 1/N
     ## Inicializar Q-table
     if Q_LEARNING_RESET:
@@ -49,13 +47,6 @@ while SMART_SAMPLING_N > 1:
         if reward is not None:
             reward = float(parser.get_reward())
             games += 1
-
-            if reward == -1:
-                result_smartsampling += 1
-            elif reward == 1:
-                result_qlearning += 1
-            else:
-                result_draw += 0
 
             parser.reset_vars()
         else:
@@ -80,12 +71,9 @@ while SMART_SAMPLING_N > 1:
         if games > 1024:
             break
 
-    with open(OUTPUT_FILE, 'ab') as f:
-        f.write(f"Q-learning vs Smart Sampling: ronda {iterarion}\n".encode())
-        f.write(f"Q-learning post-traning: \n".encode())
-        f.write(f"Q-learning: {result_qlearning}, ".encode())
-        f.write(f"Smart Sampling: {result_smartsampling},".encode())
-        f.write(f"Empates: {result_draw}\n".encode())
+    # Guardar resultados
+
+    OUTPUTTER.run_test(Q_TABLE, SMART_SAMPLING, parser, SMART_SAMPLING_CONSTANT)
 
     # Correr las N estrategias de SmartSampling con Q-learning entrenado
     z_reward = []
@@ -124,25 +112,6 @@ while SMART_SAMPLING_N > 1:
         
         #print("Z:", z, ": Recompensa obtenida:", reward)
         z_reward.append({'z': z, 'r': reward})
-    
-    # Guardar resultados
-    result_qlearning = 0
-    result_smartsampling = 0
-    result_draw = 0
-    for z in z_reward:
-        if z['r'] == -1:
-            result_smartsampling += 1
-        elif z['r'] == 1:
-            result_qlearning += 1
-        else:
-            result_draw += 1
-
-    with open(OUTPUT_FILE, 'ab') as f:
-        f.write(f"Smart Sampling post-training:\n".encode())
-        f.write(f"Q-learning: {result_qlearning}, ".encode())
-        f.write(f"Smart Sampling: {result_smartsampling}, ".encode())
-        f.write(f"Empates: {result_draw}\n".encode())
-        f.write(b"\n")
 
     # Actualizar Smart Sampling
     ## Ordenamos z_reward por recompensa
@@ -154,139 +123,11 @@ while SMART_SAMPLING_N > 1:
     z_keys = [z['z'] for z in z_reward]
     SMART_SAMPLING.update_zs(z_keys)
 
+    OUTPUTTER.run_test(Q_TABLE, SMART_SAMPLING, parser, SMART_SAMPLING_CONSTANT)
+
     iterarion +=1
     #Q_TABLE.display_q_table()
 
-
-# POST-TRAINING
-# Comparativa con ambos entrenados
-assert len(SMART_SAMPLING.zs) == 1
-
-result_qlearning = 0
-result_smartsampling = 0
-result_draw = 0
-
-SMART_SAMPLING_ITERATIONS = END_COMPARATION // 2
-Q_LEARNING_ITERATIONS = END_COMPARATION - SMART_SAMPLING_ITERATIONS
-
-z = SMART_SAMPLING.zs[0]
-print("Z final:", z)
-for i in range(SMART_SAMPLING_ITERATIONS):
-    reward = None
-    parser.reset_vars()
-    
-    j = 0
-    while reward == None and j < 1000:
-        reward = parser.get_reward()
-        vars = parser.get_vars()
-        
-        if reward is not None:
-            reward = float(reward)
-            break
-        
-        else:
-            options = parser.get_options(vars)
-            if parser.get_var("t") == "0":
-                action = Q_TABLE.choose_action(vars, parser.get_actions())
-                #print("En estado ", vars, "Q-learning eligió la acción", action)
-                parser.update_vars(action, options)
-            elif parser.get_var("t") == "1":
-                hash_value = SMART_SAMPLING.hash(vars, z)
-                action = SMART_SAMPLING.choose_action(hash_value, parser.get_actions())
-                #print("En estado ", vars, "SmartSampling eligió la acción", action)
-                parser.update_vars(action, options)
-            else:
-                assert True
-        
-        j += 1
-
-    if reward == -1:
-        result_smartsampling += 1
-    elif reward == 1:
-        result_qlearning += 1
-    else:
-        result_draw += 0
-
-with open(OUTPUT_FILE, 'ab') as f:
-    f.write(f"Q-LEARNING vs SMARTSAMPLING\n".encode())
-    f.write(f"Smart Sampling fue el último entrenado\n".encode())
-    f.write(f"Q-learning ganó {result_qlearning} veces\n".encode())
-    f.write(f"Smart Sampling ganó {result_smartsampling} veces\n".encode())
-    f.write(f"Hubo {result_draw} empates\n".encode())
-    f.write(f"\n".encode())
-
-
-Q_TABLE.init_q_table(parser.get_all_posibilities())
-
-## Correr Q-learning
-for _ in range(Q_TABLE_ITERATIONS):
-    vars = parser.get_vars()
-    reward = parser.get_reward()
-    action = '__None__'
-
-    if reward is not None:
-        reward = float(parser.get_reward())
-        parser.reset_vars()
-
-    else:
-        reward = 0
-        options = parser.get_options(vars)
-        
-        if parser.get_var("t") == "0":
-            action = Q_TABLE.choose_action(vars, parser.get_actions())
-            parser.update_vars(action, options)
-
-        elif parser.get_var("t") == "1":
-            hash_value = SMART_SAMPLING.hash(vars, z)
-            action = SMART_SAMPLING.choose_action(hash_value, parser.get_actions())
-            parser.update_vars(action, options)
-
-        else:
-            assert True
-            
-    Q_TABLE.update_column(vars, action, parser.get_vars(), reward)
-
-result_qlearning = 0
-result_smartsampling = 0
-result_draw = 0
-for i in range(Q_LEARNING_ITERATIONS):
-    reward = None
-    parser.reset_vars()
-    
-    j = 0
-    while reward == None and j < 1000:
-        reward = parser.get_reward()
-        vars = parser.get_vars()
-        
-        if reward is not None:
-            reward = float(reward)
-            break
-        
-        else:
-            options = parser.get_options(vars)
-            if parser.get_var("t") == "0":
-                action = Q_TABLE.choose_action(vars, parser.get_actions())
-                #print("En estado ", vars, "Q-learning eligió la acción", action)
-                parser.update_vars(action, options)
-            elif parser.get_var("t") == "1":
-                hash_value = SMART_SAMPLING.hash(vars, z)
-                action = SMART_SAMPLING.choose_action(hash_value, parser.get_actions())
-                #print("En estado ", vars, "SmartSampling eligió la acción", action)
-                parser.update_vars(action, options)
-            else:
-                assert True
-        
-        j += 1
-
-    if reward == -1:
-        result_smartsampling += 1
-    elif reward == 1:
-        result_qlearning += 1
-    else:
-        result_draw += 0
-
-with open(OUTPUT_FILE, 'ab') as f:
-    f.write(f"Q-learning fue el último entrenado\n".encode())
-    f.write(f"Q-learning ganó {result_qlearning} veces\n".encode())
-    f.write(f"Smart Sampling ganó {result_smartsampling} veces\n".encode())
-    f.write(f"Hubo {result_draw} empates\n".encode())
+OUTPUTTER.print("Entrenamiento terminado. Corriendo 3 juegos de prueba.")
+for _ in range(3):
+    OUTPUTTER.run_test(Q_TABLE, SMART_SAMPLING, parser, SMART_SAMPLING_CONSTANT)
---
+++ training_output.py
@@ -0,0 +1,71 @@
+import os
+
+from parser import Parser
+from qlearning.main import Q_table
+from smartsampling.main import Transformer as SmartSampling
+
+OUTPUT_DIR = "output/"
+
+class TrainingOutput:
+    def __init__(self, output_file):
+        # Si el outputfile no existe, crearlo
+        output_file = OUTPUT_DIR + output_file + ".txt"
+        if os.path.exists(output_file):
+            os.remove(output_file)
+        open(output_file, 'a').close()
+        self.output_file = output_file
+    
+    def run_test(self, q_table: Q_table, smartsampling: SmartSampling, parser: Parser, ss_contant: bool):
+        ROUNDS = 1000
+
+        result_q = 0
+        result_ss = 0
+        result_draw = 0
+
+        z = smartsampling.get_random_z()
+
+        for _ in range(ROUNDS):
+            reward = None
+            parser.reset_vars()
+    
+            j = 0
+            while reward == None and j < 1000:
+                reward = parser.get_reward()
+                vars = parser.get_vars()
+                
+                if reward is not None:
+                    reward = float(reward)
+                    break
+                
+                else:
+                    options = parser.get_options(vars)
+                    if parser.get_var("t") == "0":
+                        action = q_table.choose_action(vars, parser.get_actions())
+                        #print("En estado ", vars, "Q-learning eligió la acción", action)
+                        parser.update_vars(action, options)
+                    elif parser.get_var("t") == "1":
+                        if not ss_contant:
+                            z = smartsampling.get_random_z()
+                        hash_value = smartsampling.hash(vars, z)
+                        action = smartsampling.choose_action(hash_value, parser.get_actions())
+                        #print("En estado ", vars, "SmartSampling eligió la acción", action)
+                        parser.update_vars(action, options)
+                    else:
+                        print("Turno invalido:", parser.get_var("t"))
+                        assert True
+                
+                j += 1
+            
+            
+            if reward == 1: result_q += 1
+            elif reward == -1: result_ss += 1
+            else: result_draw += 1
+        
+        output = f"Q-learning: {result_q/10}%, SmartSampling: {result_ss/10}% y {result_draw/10}% empates."
+
+        with open(self.output_file, 'a') as f:
+            f.write(output + "\n")
+
+    def print(self, text: str):
+        with open(self.output_file, 'a') as f:
+            f.write(text + "\n")
